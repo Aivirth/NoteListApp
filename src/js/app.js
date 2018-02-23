c6535b514b9fd30192a1a5a2641365f6e00144ad
Aivirth <aivirth.work@gmail.com>
2018-02-23 17:55:11 +0100

    Created HTTP Promise Based helper library

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,26 @@
 import '../css/style.scss'
 import {http} from './httplibrary';
+
+/*
+http.get('https://jsonplaceholder.typicode.com/users')
+    .then(data =>console.log(data))
+    .catch(err => console.log(err));
+*/
+
+const data = {
+    name : 'john doe',
+    username : 'johndoe',
+    email :'text@test.com'
+}
+
+/*
+http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data =>console.log(data))
+    .catch(err => console.log(err));
+*/
+
+/*
+http.put('https://jsonplaceholder.typicode.com/users/1', data)
+    .then(data =>console.log(data))
+    .catch(err => console.log(err));
+*/
--- src/js/httplibrary.js
+++ src/js/httplibrary.js
@@ -1,7 +1,60 @@
 class HTTP{
-    constructor(){
-        console.log('working');
+    //make http get request
+    get(url){
+        return new Promise((resolve, reject)=>{
+            fetch(url)
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        })        
     }
+
+    //make http post request
+    post(url, data){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method : 'POST',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body : JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        })        
+    }
+
+    //make http put request
+    put(url, data){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method : 'PUT',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+                body : JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        })        
+    }
+    //make http delete request
+    put(url){
+        return new Promise((resolve, reject)=>{
+            fetch(url, {
+                method : 'DELETE',
+                headers : {
+                    'Content-type' : 'application/json'
+                }
+            })
+                .then(res => res.json())
+                .then(() => resolve('Resource Deleted'))
+                .catch(err => reject(err));
+        })        
+    }
+
 }
 
 export const http = new HTTP();
